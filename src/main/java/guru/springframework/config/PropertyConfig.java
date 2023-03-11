c6f0995836a77297b3175c278b6d7b7c060fa2f1
Dylane-Agoka <dylaneagoka47@gmail.com>
2023-03-11 11:22:54 +0100

    Introduction to YAML

--- src/main/java/guru/springframework/config/PropertyConfig.java
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -5,7 +5,6 @@ import guru.springframework.examplebeans.FakeJmsBroker;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 public class PropertyConfig {
